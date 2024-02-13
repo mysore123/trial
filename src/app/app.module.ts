2bc869559a5dcd409a1eb0903b7ac18b6e3220c3
LTPB\U6074388 <anirudh.prakashmysore@clarivate.com>
2024-02-13 21:12:50 +0530

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { QuestionComponent } from './question/question.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CongratsComponent } from './congrats/congrats.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'question', component:  QuestionComponent},
+  { path: 'congrats', component: CongratsComponent}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    QuestionComponent
+    QuestionComponent,
+    CongratsComponent
   ],
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  exports: [RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-question',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
+  yesButtonWidth = 100;
+  yesButtonHeight = 56.8;
+  noButtonTextOptions = [
+    'No',
+    'Are you sure',
+    'I got only one heart girl😔',
+    'This has to be a mistake',
+    'Last Chance 😭',
+  ];
+  noButtonText = this.noButtonTextOptions[0];
+  index = 0;
 
-  constructor() { }
+  noButtonWidth = 50;
+  noButtonFontSize = 18;
+
+  constructor(private router: Router) { 
+
+  }
 
   ngOnInit(): void {
   }
+
+  public handleYesClick() {
+    this.router.navigate(['/congrats']);
+  }
+
+  public handleNoClick() {
+    if (this.yesButtonHeight < 200 && this.yesButtonWidth < 200) {
+      this.yesButtonWidth += 20; 
+      this.yesButtonHeight += 20;
+    }
+
+    this.index = this.index >= 4 ? 0 : this.index + 1;
+    this.noButtonText =   this.noButtonTextOptions[this.index];
+
+    if (this.noButtonFontSize > 11) {
+      this.noButtonWidth = this.noButtonWidth == 20 ? 20 : this.noButtonWidth - 10;
+      this.noButtonFontSize = this.noButtonFontSize - 2;
+    }
+    
+  }
+    
 
 }
